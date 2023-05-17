30a52c4e3009a3217dd0a5ad71b7f3715862d866
DuyKhoi03 <125723528+DuyKhoi03@users.noreply.github.com>
2023-05-17 17:11:12 +0700

    Add files via upload

---
+++ SortStudentByGPA.java
@@ -0,0 +1,14 @@
+package th.buoi3.cau3;
+
+import java.util.Comparator;
+
+
+public class SortStudentByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        if (student1.getGpa() > student2.getGpa()) {
+            return 1;
+        }
+        return -1;
+    }
+}
